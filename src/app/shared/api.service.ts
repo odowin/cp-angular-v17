2d34b776f1d667ba30561c19ac243d813ddf0174
odowin <102875944+odowin@users.noreply.github.com>
2024-11-18 12:04:57 +0100

    its ok

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Cupcake } from './cupcake.model'; // Modèle Cupcake
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  private apiUrl = 'http://localhost:4000/cupcakes'; // URL de l'API
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  // Méthode pour récupérer les cupcakes
+  getCupcakes(): Observable<Cupcake[]> {
+    return this.http.get<Cupcake[]>(this.apiUrl);
+  }
 }
---
+++ src/app/shared/cupcake.model.ts
@@ -0,0 +1,10 @@
+export interface Cupcake {
+    id: string;
+    accessory_id: string;
+    url: string;
+    color1: string;
+    color2: string;
+    color3: string;
+    name: string;
+  }
+  
